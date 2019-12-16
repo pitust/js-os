20dd9682c833dcf6d538eb5d56f09f868d486c68
pitust <stelmaszek.piotrpilot@gmail.com>
2019-12-16 21:11:39 +0000

    outb => Nat_OUTB

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,12 @@
-function outb(port, val) {
-    Nat_OUTB(port, val);
-}
 let LBA = 0;
 const SECCOUNT = 1;
-outb(0x1F6, 0xE0 | ((LBA >> 24) & 0x0F));
-outb(0x1F1, 0x00);
-outb(0x1F2, SECCOUNT);
-outb(0x1F3, LBA);
-outb(0x1F4, LBA >> 8);
-outb(0x1F5, LBA >> 16);
-outb(0x1F7, 0x20);
+Nat_OUTB(0x1F6, 0xE0 | ((LBA >> 24) & 0x0F));
+Nat_OUTB(0x1F1, 0x00);
+Nat_OUTB(0x1F2, SECCOUNT);
+Nat_OUTB(0x1F3, LBA);
+Nat_OUTB(0x1F4, LBA >> 8);
+Nat_OUTB(0x1F5, LBA >> 16);
+Nat_OUTB(0x1F7, 0x20);
 while (!(Nat_INB(0x1F7) & 8));
 let buf = [];
 for (let i = 0;i < 256;i++) {
