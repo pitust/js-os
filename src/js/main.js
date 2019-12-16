5e92488d5d8cb874f45f9c642e15815172157f35
pitust <stelmaszek.piotrpilot@gmail.com>
2019-12-16 21:15:35 +0000

    Hello world written wrong, readDisk function

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,30 @@
-let LBA = 0;
 const SECCOUNT = 1;
-Nat_OUTB(0x1F6, 0xE0 | ((LBA >> 24) & 0x0F));
-Nat_OUTB(0x1F1, 0x00);
-Nat_OUTB(0x1F2, SECCOUNT);
-Nat_OUTB(0x1F3, LBA);
-Nat_OUTB(0x1F4, LBA >> 8);
-Nat_OUTB(0x1F5, LBA >> 16);
-Nat_OUTB(0x1F7, 0x20);
-while (!(Nat_INB(0x1F7) & 8));
-let buf = [];
-for (let i = 0;i < 256;i++) {
-    let val = Nat_INW(0x1F0);
-    buf[i*2] = val & 0xff;
-    buf[i*2 + 1] = val >> 8;
+function readDisk(LBA) {
+    let readBuf = [];
+    readBuf[511] = 0;
+    Nat_OUTB(0x1F6, 0xE0 | ((LBA >> 24) & 0x0F));
+    Nat_OUTB(0x1F1, 0x00);
+    Nat_OUTB(0x1F2, SECCOUNT);
+    Nat_OUTB(0x1F3, LBA);
+    Nat_OUTB(0x1F4, LBA >> 8);
+    Nat_OUTB(0x1F5, LBA >> 16);
+    Nat_OUTB(0x1F7, 0x20);
+    while (!(Nat_INB(0x1F7) & 8));
+    for (let i = 0; i < 256; i++) {
+        let val = Nat_INW(0x1F0);
+        readBuf[i * 2] = val & 0xff;
+        readBuf[i * 2 + 1] = val >> 8;
+    }
+    return readBuf;
 }
-for (let i = 0;i < 512;i++) {
+let buf = readDisk(0), lr = 0;
+for (let i = 0; i < 512; i++) {
     let v = buf[i];
-    if (v == 0)break;
+    if (v == 0) break;
     Nat_PUTC(v);
+    if (i == 511) {
+        buf = readDisk(++lr);
+        i = 0;
+    }
 }
 console.log('\nDONE');
